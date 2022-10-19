a0657a7b874116551c277605e52801cb7908743b
Rashad Abdul-Salaam <upennmobile@gmail.com>
2022-10-19 01:52:41 -0400

    Configure StartupCoordinator

---
+++ SpaceXAPI/Coordinators/SXStartupCoordinator.swift
@@ -0,0 +1,48 @@
+//
+//  SXStartupCoordinator.swift
+//  SpaceXAPI
+//
+//  Created by Rashad Abdul-Salam on 10/19/22.
+//
+
+import Foundation
+import UIKit
+
+/// Object that encapsulates all primary Master-Detail ViewController dependency configuration
+final class SXStartupCoordinator {
+    
+    /// Enum providing human-readable Int-values for Master-Details VCs
+    enum SplitChildVCs : Int {
+        case Master, Details
+    }
+    
+    var window: UIWindow?
+    var launchProvider : SXLaunchProviderService
+    
+    init(window: UIWindow?, launchProvider: SXLaunchProviderService = SXLaunchProviderService()) {
+        self.window = window
+        self.launchProvider = launchProvider
+    }
+    
+    func start() {
+        self.getLaunchData()
+    }
+}
+
+private extension SXStartupCoordinator {
+    func getLaunchData() {
+        guard
+          let splitVC = window?.rootViewController as? SXSplitViewController,
+            let masterVC = (splitVC.viewControllers[SplitChildVCs.Master.rawValue] as? UINavigationController)?.topViewController as? SXLaunchesViewController,
+            let detailsVC = (splitVC.viewControllers[SplitChildVCs.Details.rawValue] as? UINavigationController)?.topViewController as? SXLaunchDetailsViewController
+          else { fatalError() }
+        masterVC.delegate = detailsVC
+        self.launchProvider.getLaunchData { launches, error in
+            DispatchQueue.main.async {
+                let firstLaunch = launches.first
+                masterVC.launches = launches
+                detailsVC.launch = firstLaunch
+            }
+        }
+    }
+}
--- SpaceXAPI/SceneDelegate.swift
+++ SpaceXAPI/SceneDelegate.swift
@@ -11,58 +11,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-    /**
-     Startup Coordinator Methods
-     */
-    var launchProvider = SXLaunchProviderService()
-
-    /// Enum providing human-readable Int-values for Master-Details VCs
-    enum SplitChildVCs : Int {
-        case Master, Details
-    }
-    
-//    var masterVC : SXLaunchesViewController?
-//    var detailsVC : SXLaunchDetailsViewController?
-    
-    func getLaunchData() {
-        guard
-          let splitVC = window?.rootViewController as? SXSplitViewController,
-            let masterVC = (splitVC.viewControllers[SplitChildVCs.Master.rawValue] as? UINavigationController)?.topViewController as? SXLaunchesViewController,
-            let detailsVC = (splitVC.viewControllers[SplitChildVCs.Details.rawValue] as? UINavigationController)?.topViewController as? SXLaunchDetailsViewController
-          else { fatalError() }
-        masterVC.delegate = detailsVC
-        self.launchProvider.getLaunchData { launches, error in
-//            self?.launches = launches
-            DispatchQueue.main.async {
-                let firstLaunch = launches.first
-                masterVC.launches = launches
-                detailsVC.launch = firstLaunch
-            }
-        }
-    }
-    
-    
-    
-    // ***** END Coordinator Methods
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        self.getLaunchData()
-//        guard
-//          let splitVC = window?.rootViewController as? SXSplitViewController,
-////          let leftNavController = splitViewController.viewControllers.first
-////            as? UINavigationController,
-//            let masterVC = (splitVC.viewControllers[ChildVCIdx.Master.rawValue] as? UINavigationController)?.topViewController as? SXLaunchesViewController,
-////          let masterViewController = leftNavController.viewControllers.first
-////            as? SXLaunchesViewController,
-////          let detailViewController = splitViewController.viewControllers.last
-////            as? SXLaunchDetailsViewController
-//            let detailsVC = splitVC.viewControllers[ChildVCIdx.Details.rawValue] as? SXLaunchDetailsViewController
-//          else { fatalError() }
-
-//        let firstMonster = masterViewController.monsters.first
-//        detailViewController.monster = firstMonster
+        let startupCoordinator = SXStartupCoordinator(window: self.window)
+        startupCoordinator.start()
         
         guard let _ = (scene as? UIWindowScene) else { return }
     }
